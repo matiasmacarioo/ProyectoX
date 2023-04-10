58e153fa79a6d307ae8acbb3b41dd40e25ac3fbc
matiasmacarioo <matiasmacarioo@gmail.com>
2023-04-10 20:29:56 -0300

    ui

--- Areas/Identity/Pages/Account/Manage/ChangePassword.cshtml.cs
+++ Areas/Identity/Pages/Account/Manage/ChangePassword.cshtml.cs
@@ -54,7 +54,7 @@ namespace ProyectoX.Areas.Identity.Pages.Account.Manage
             /// </summary>
             [Required]
             [DataType(DataType.Password)]
-            [Display(Name = "Current password")]
+            [Display(Name = "Contraseña actual")]
             public string OldPassword { get; set; }
 
             /// <summary>
@@ -64,7 +64,7 @@ namespace ProyectoX.Areas.Identity.Pages.Account.Manage
             [Required]
             [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
             [DataType(DataType.Password)]
-            [Display(Name = "New password")]
+            [Display(Name = "Nueva contraseña")]
             public string NewPassword { get; set; }
 
             /// <summary>
@@ -72,7 +72,7 @@ namespace ProyectoX.Areas.Identity.Pages.Account.Manage
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
             [DataType(DataType.Password)]
-            [Display(Name = "Confirm new password")]
+            [Display(Name = "Confirmar nueva contraseña")]
             [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
             public string ConfirmPassword { get; set; }
         }
