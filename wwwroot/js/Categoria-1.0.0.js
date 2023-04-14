79f1c8b60c9b79a83d33e3f18def889fac84d9ea
matiasmacarioo <matiasmacarioo@gmail.com>
2023-04-13 21:28:19 -0300

    ui

--- wwwroot/js/Categoria-1.0.0.js
+++ wwwroot/js/Categoria-1.0.0.js
@@ -3,41 +3,32 @@ window.onload = BuscarCategorias();
 function BuscarCategorias(){
     $("#tbody-categorias").empty();
     $.ajax({
-        // la URL para la petición
-        url : '../../Categorias/BuscarCategorias',
-        // la información a enviar
-        // (también es posible utilizar una cadena de datos)
-        data : {  },    
-        // especifica si será una petición POST o GET
-        type : 'GET',
-        // el tipo de información que se espera de respuesta
-        dataType : 'json',
-        // código a ejecutar si la petición es satisfactoria;
-        // la respuesta es pasada como argumento a la función
-        success : function(categorias) {
-            $("#tbody-categorias").empty();
-            $.each(categorias, function( index, categoria ) {
-                $("#tbody-categorias").append(
-                    '<tr>' +
-                        '<td> <a class="btn btn-primary btn-sm" onClick="BuscarCategoria(' + categoria.categoriaID + ')" role="button">' + categoria.descripcion + '</a></td>' +
-                    '</tr>'
-                );     
-            });
-        },
-   
-        // código a ejecutar si la petición falla;
-        // son pasados como argumentos a la función
-        // el objeto de la petición en crudo y código de estatus de la petición
-        error : function(xhr, status) {
-            alert('Error al cargar categorias');
-        },
-   
-        // código a ejecutar sin importar si la petición falló o no
-        complete : function(xhr, status) {
-            //alert('Petición realizada');
-        }
+      url : '../../Categorias/BuscarCategorias',
+      data : {},
+      type : 'GET',
+      dataType : 'json',
+      success : function(categorias) {
+        $("#tbody-categorias").empty();
+        $.each(categorias, function(index, categoria) {
+          $("#tbody-categorias").append(`
+            <tr>
+              <td class="text-light " >${categoria.descripcion}</td>
+              <td class="text-light text-center">
+                <button class="btn btn-dark btn-sm" onClick="BuscarCategoria(${categoria.categoriaID})">Editar</button>
+              </td>
+            </tr>
+          `);
+        });
+      },
+      error : function(xhr, status) {
+        alert('Error al cargar categorias');
+      },
+      complete : function(xhr, status) {
+        //alert('Petición realizada');
+      }
     });
-}
+  }
+  
 
 function VaciarFormulario(){
     $("#Descripcion").val('');
