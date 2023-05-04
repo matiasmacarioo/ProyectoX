52077e208eeb124c45cab825e96b232dcd6306f1
matiasmacarioo <matiasmacarioo@gmail.com>
2023-05-04 01:15:19 -0300

    explicacion controlador servicios

--- Controllers/ServiciosController.cs
+++ Controllers/ServiciosController.cs
@@ -30,6 +30,7 @@ public class ServiciosController : Controller
     {
         int resultado = 0;
 
+        // Si el id del producto recibido no es cero
         if (productoID != 0)
         {
             // comprueba que la descripcion recibida no este vacia.
@@ -40,14 +41,15 @@ public class ServiciosController : Controller
                     if (!string.IsNullOrEmpty(telefono))
                     {
 
-                        // crea un nuevo servicio si su id es 0
+                        // si el id del servicio es 0, crea un nuevo servicio 
                         if (servicioID == 0)
                         {
-                            // Comprueba si no existe otro con la misma descripcion
+                            // Comprueba si no existe otro servicio con la misma descripcion
                             var servicioOriginal = _contexto.Servicios.Where(p => p.Descripcion == descripcion && p.ServicioID != servicioID).FirstOrDefault();
-                            // si no hay uno igual procede a crear un nuevo servicio
+                            // si no hay otro servicio igual procede a crear uno nuevo
                             if (servicioOriginal == null)
                             {
+                                // crea un nuevo objeto llamado servicio y guarda sus atributos
                                 var servicioGuardar = new Servicio
                                 {
                                     Descripcion = descripcion,
@@ -61,19 +63,20 @@ public class ServiciosController : Controller
                             }
                             else
                             {
-                                resultado = 1;
+                                resultado = 1; // 1 indica que ya existe un servicio con la misma descripcion
                             }
                         }
                         else
                         // edita un servicio ya creado ya que el id no es 0
                         {
-                            // comprueba que el nombre del servicio sea diferente a otros en la misma categoría
+                            // comprueba que la descripcion del servicio editado no sea igual a otro en la misma categoria
                             var servicioOriginal = _contexto.Servicios.Where(p => p.Descripcion == descripcion && p.ServicioID != servicioID).FirstOrDefault();
                             if (servicioOriginal == null)
                             {
                                 var servicioEditar = _contexto.Servicios.Find(servicioID);
                                 if (servicioEditar != null)
                                 {
+                                    // edita los atributos del objeto Servicio ya existente 
                                     servicioEditar.Descripcion = descripcion;
                                     servicioEditar.Direccion = direccion;
                                     servicioEditar.Telefono = telefono;
@@ -84,29 +87,29 @@ public class ServiciosController : Controller
                             }
                             else
                             {
-                                resultado = 1;
+                                resultado = 1; // 1 indica que ya existe un servicio con la misma descripcion
                             }
                         }
                     }
                     else
                     {
-                        resultado = 5;
+                        resultado = 5; // 5 indica que el campo telefono esta vacio
                     }
                 }
                 else
                 {
-                    resultado = 4;
+                    resultado = 4; // 4 indica que el campo direccion esta vacio
                 }
             }
             else
             {
-                resultado = 2;
+                resultado = 2; // 2 indica que el campo descripcion esta vacio
             }
 
         }
         else
         {
-            resultado = 3;
+            resultado = 3; // 3 indica que el productoID es cero
         }
 
         return Json(resultado);
@@ -117,21 +120,25 @@ public class ServiciosController : Controller
     {
         List<VistaServicio> ServiciosMostrar = new List<VistaServicio>();
 
-        // busca los servicios en productos habilitadas y las ordena por descripcion
+        // Busca todos los servicios en productos habilitados en categorias habilitadas y los ordena por descripción
         var Servicios = _contexto.Servicios.Include(s => s.Producto).Where(s => s.Producto.Eliminado == false && s.Producto.Categoria.Eliminado == false).OrderBy(p => p.Descripcion).ToList();
 
-                if (!User.Identity.IsAuthenticated)
+        if (!User.Identity.IsAuthenticated)
         {
-        // Si el usuario no está autenticado, filtrar solo las categorías permitidas
-        Servicios = _contexto.Servicios.Include(s => s.Producto).Where(s => s.Producto.Eliminado == false && s.Producto.Categoria.Eliminado == false).Where(s => s.Eliminado == false).OrderBy(p => p.Descripcion).ToList();
+            // Si el usuario no está autenticado, filtra solo las servicios habilitados en productos habilitados en categorias habilitadas y los ordena por descripción
+            Servicios = _contexto.Servicios.Include(s => s.Producto).Where(s => s.Producto.Eliminado == false && s.Producto.Categoria.Eliminado == false).Where(s => s.Eliminado == false).OrderBy(p => p.Descripcion).ToList();
         }
 
         if (ServicioID > 0)
         {
+            // Si se especificó un ServicioID, filtra la lista de servicios para mostrar solo ese servicio
             Servicios = Servicios.Where(s => s.ServicioID == ServicioID).OrderBy(s => s.Descripcion).ToList();
         }
+
         foreach (var Servicio in Servicios)
         {
+            // Crea un objeto VistaServicio con la información del servicio a mostrar
+            // Esta instancia es creada para que en la vista de servicios se pueda ver la descripcion del producto al que corresponde.
             var ServicioMostrar = new VistaServicio
             {
                 ServicioID = Servicio.ServicioID,
@@ -141,8 +148,8 @@ public class ServiciosController : Controller
                 Eliminado = Servicio.Eliminado,
                 ProductoID = Servicio.ProductoID,
                 ProductoDescripcion = Servicio.Producto.Descripcion
-
             };
+            // Agrega el objeto VistaServicio a la lista ServiciosMostrar
             ServiciosMostrar.Add(ServicioMostrar);
         }
 
@@ -151,22 +158,22 @@ public class ServiciosController : Controller
 
     public JsonResult BuscarProductos()
     {
+        // Obtiene todos los productos y los ordena por descripción
         var productos = _contexto.Productos.OrderBy(c => c.Descripcion).ToList();
         return Json(productos);
     }
 
-
     [Authorize]
-
     public JsonResult DeshabilitarServicio(int servicioID)
     {
         bool resultado = true;
         if (servicioID != 0)
         {
-            //crear variable que guarde el objeto segun el id deseado
+            // Busca el servicio correspondiente al servicioID dado
             var servicioDeshabilitar = _contexto.Servicios.Find(servicioID);
             if (servicioDeshabilitar != null)
             {
+                // Deshabilita el servicio y guarda los cambios en la base de datos
                 servicioDeshabilitar.Eliminado = true;
                 _contexto.SaveChanges();
                 resultado = true;
@@ -180,15 +187,16 @@ public class ServiciosController : Controller
     }
 
     [Authorize]
-
     public JsonResult HabilitarServicio(int servicioID)
     {
         bool resultado = true;
         if (servicioID != 0)
         {
+            // Busca el servicio correspondiente al servicioID dado
             var servicioHabilitar = _contexto.Servicios.Find(servicioID);
             if (servicioHabilitar != null)
             {
+                // Habilita el servicio y guarda los cambios en la base de datos
                 servicioHabilitar.Eliminado = false;
                 _contexto.SaveChanges();
                 resultado = true;
@@ -207,10 +215,12 @@ public class ServiciosController : Controller
         bool resultado = true;
         if (servicioID != 0)
         {
-            var servicioDeshabilitar = _contexto.Servicios.Find(servicioID);
-            if (servicioDeshabilitar != null)
+            // Busca el servicio correspondiente al servicioID dado
+            var servicioEliminar = _contexto.Servicios.Find(servicioID);
+            if (servicioEliminar != null)
             {
-                _contexto.Servicios.Remove(servicioDeshabilitar);
+                // Elimina el servicio de la base de datos
+                _contexto.Servicios.Remove(servicioEliminar);
                 _contexto.SaveChanges();
                 resultado = true;
             }
